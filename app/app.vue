47275a1878c442e2263a09a3029c3705c3980fa9
asib-hasan <asib.uucse@gmail.com>
2026-07-09 11:03:01 +0600

    updated

--- app/app.vue
+++ app/app.vue
@@ -6,8 +6,9 @@ colorMode.preference = 'light'
 </script>
 
 <template>
-  <NuxtLoadingIndicator />
+  <NuxtLoadingIndicator color="repeating-linear-gradient(to right,#00dc82 0%,#34cdfe 50%,#0047e1 100%)" :height="4" :duration="3000" :throttle="100" />
   <NuxtLayout>
     <NuxtPage />
   </NuxtLayout>
+  <UNotifications />
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,7 +6,27 @@ export default defineNuxtConfig({
 
   app: {
     baseURL: '/',
-    buildAssetsDir: '/_nuxt/'
+    buildAssetsDir: '/_nuxt/',
+    head: {
+      htmlAttrs: { lang: 'en' },
+      charset: 'utf-8',
+      viewport: 'width=device-width, initial-scale=1',
+      titleTemplate: '%s - HomeoDoctors BD',
+      meta: [
+        { name: 'description', content: 'Find the best homeopathic doctors in Bangladesh. Consult online or book appointments easily.' }
+      ]
+    },
+    pageTransition: { name: 'page', mode: 'out-in' },
+    layoutTransition: { name: 'layout', mode: 'out-in' }
+  },
+
+  routeRules: {
+    '/': { isr: 60 * 60 },
+    '/doctors/**': { isr: 60 * 60 },
+    '/blog/**': { isr: 60 * 60 },
+    '/contact': { prerender: true },
+    '/about': { prerender: true },
+    '/api/**': { cors: true, headers: { 'access-control-allow-methods': 'GET,POST' } },
   },
 
   colorMode: {
@@ -18,8 +38,8 @@ export default defineNuxtConfig({
 
   runtimeConfig: {
     public: {
-      // apiBaseUrl: 'http://127.0.0.1:8000/api' 
-      apiBaseUrl: 'https://admin.homeodoctorsbd.com/api'
+      apiBaseUrl: 'http://127.0.0.1:8000/api'
+      // apiBaseUrl: 'https://admin.homeodoctorsbd.com/api'
     }
   },
 
@@ -29,7 +49,8 @@ export default defineNuxtConfig({
       Outfit: [300, 400, 500, 600, 700, 800],
       'Plus Jakarta Sans': [400, 500, 600, 700],
     },
-    display: 'swap',
+    display: 'block', // 'block' prevents FOUT (shaking) by hiding text until font loads
+    download: true,
     prefetch: true,
     preconnect: true,
     preload: true,
